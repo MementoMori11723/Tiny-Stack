55085355c8c3a8db572971ab12ad8b560bb314f2
yasasvi <yasasvigumma@gmail.com>
2024-06-29 20:07:38 +0530

    made a server that can quit on pressing 'q' key

--- src/main.go
+++ src/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 func main() {
@@ -29,13 +33,35 @@ func main() {
 			_404.Render(context.Background(), w)
 		},
 	}
-	fmt.Println("App running at http://localhost:5000")
-	http.ListenAndServe(":5000", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	server := &http.Server{Addr: ":5000", Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		handler, ok := handlers[r.URL.Path]
 		if !ok {
 			handlers["/404"](w, r)
 			return
 		}
 		handler(w, r)
-	}))
+	})}
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("ListenAndServe(): ", err)
+		}
+	}()
+	fmt.Println("Server is running on http://localhost:5000")
+	fmt.Println("Press 'q' and 'Enter' to stop the server")
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input := scanner.Text()
+		if input == "q" {
+			break
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Printf("Error reading from input: %v", err)
+	}
+	fmt.Println("Shutting down the server...")
+	if err := server.Close(); err != nil {
+		fmt.Printf("Error shutting down server: %v\n", err)
+	}
+	time.Sleep(1 * time.Second)
+	fmt.Println("Server stopped.")
 }
