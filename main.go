491fe7e65e945fdb851718082188b94400accf6b
yasasvi <yasasvigumma@gmail.com>
2024-08-20 12:38:15 +0530

    Handled few issues
    made it easy to read!

--- main.go
+++ main.go
@@ -2,28 +2,36 @@ package main
 
 import (
 	"fmt"
-	"tiny/stack/pages"
 	"net/http"
 	"os"
+	"tiny/stack/pages"
 
 	"github.com/joho/godotenv"
 )
 
-func main() {
-  err := godotenv.Load()
-  if err != nil {
-    fmt.Println("Error loading .env file")
-  }
-  PORT := os.Getenv("PORT")
+var PORT string
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+	PORT = os.Getenv("PORT")
   if PORT == "" {
-    fmt.Println("PORT not found in .env file, using default port 8080")
     PORT = "8080"
   }
+}
+
+func main() {
 	for path, handler := range pages.Routes {
 		http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprint(w, handler())
+			if r.URL.Path != path {
+				http.Redirect(w, r, "/error", http.StatusSeeOther)
+			}
+			t := handler()
+			t.Execute(w, nil)
 		})
 	}
-  fmt.Println("Server is running on port:", PORT)
-  http.ListenAndServe(":"+PORT, nil)
+	fmt.Println("Server is running on port:", PORT)
+	http.ListenAndServe(":"+PORT, nil)
 }
--- pages/routes.go
+++ pages/routes.go
@@ -1,9 +1,23 @@
 package pages
 
-type Route map[string]func() string
+import (
+	"html/template"
+)
+
+type Route map[string]func() *template.Template
+
+func renderTemplate(path string) *template.Template {
+	return template.Must(template.ParseFiles(path))
+}
 
 var Routes = Route{
-  "/": func() string {
-    return "Hello, World!"
-  },
+	"/": func() *template.Template {
+		return renderTemplate("pages/index.html")
+	},
+	"/about": func() *template.Template {
+		return renderTemplate("pages/about.html")
+	},
+	"/error": func() *template.Template {
+		return renderTemplate("pages/error.html")
+	},
 }
