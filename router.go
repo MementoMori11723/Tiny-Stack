de63c909e1f140f185ef12bd5e10bb59250da837
yasasvi <yasasvigumma@gmail.com>
2024-08-23 19:33:55 +0530

    added a small change
    now it's a bit better!

--- router.go
+++ router.go
@@ -4,42 +4,42 @@ import (
 	"net/http"
 )
 
+func addTemplate(tmplt string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		RenderTemplate(tmplt, w)
+	}
+}
+
 func DefaultRoutes() *http.ServeMux {
 	routes := http.NewServeMux()
-  routeMapper := map[string]func(w http.ResponseWriter, r *http.Request){
-    "/": func(w http.ResponseWriter, r *http.Request) {
-      RenderTemplate("home",w)
-    },
-    "/about": func(w http.ResponseWriter, r *http.Request) {
-      RenderTemplate("about",w)
-    },
-    "/contact": func(w http.ResponseWriter, r *http.Request) {
-      RenderTemplate("contact",w)
-    },
-    "/static/": func(w http.ResponseWriter, r *http.Request) {
-      http.StripPrefix("/static/", http.FileServer(http.Dir("static"))).ServeHTTP(w, r)
-    },
-  }
-  for path, handler := range routeMapper {
-    routes.HandleFunc(path, handler)
-  }
-  return routes
+	routeMapper := map[string]func(w http.ResponseWriter, r *http.Request){
+		"/":        addTemplate("home"),
+		"/about":   addTemplate("about"),
+		"/contact": addTemplate("contact"),
+		"/static/": func(w http.ResponseWriter, r *http.Request) {
+			http.StripPrefix("/static/", http.FileServer(http.Dir("static"))).ServeHTTP(w, r)
+		},
+	}
+	for path, handler := range routeMapper {
+		routes.HandleFunc(path, handler)
+	}
+	return routes
 }
 
 func ApiRoutes() *http.ServeMux {
-  routes := http.NewServeMux()
-  routeMapper := map[string]func(w http.ResponseWriter, r *http.Request){
-    "/": func(w http.ResponseWriter, r *http.Request) {
-      w.Header().Set("Content-Type", "application/json")
-      w.Write([]byte("{'message' : 'API endpoint'}"))
-    },
-    "/admin": func(w http.ResponseWriter, r *http.Request) {
-      w.Header().Set("Content-Type", "application/json")
-      w.Write([]byte("{'message':'Admin endpoint'}"))
-    },
-  }
-  for path, handler := range routeMapper {
-    routes.HandleFunc(path, handler)
-  }
-  return routes
+	routes := http.NewServeMux()
+	routeMapper := map[string]func(w http.ResponseWriter, r *http.Request){
+		"/": func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			w.Write([]byte("{'message' : 'API endpoint'}"))
+		},
+		"/admin": func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			w.Write([]byte("{'message':'Admin endpoint'}"))
+		},
+	}
+	for path, handler := range routeMapper {
+		routes.HandleFunc(path, handler)
+	}
+	return routes
 }
