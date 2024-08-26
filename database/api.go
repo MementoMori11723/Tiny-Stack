3d022b60c2023806597c9267f343f1ec507fb577
yasasvi <yasasvigumma@gmail.com>
2024-08-26 13:08:23 +0530

    handling issues
    need to work on few other issues too!

--- database/api.go
+++ database/api.go
@@ -11,6 +11,7 @@ var (
 
 func init() {
 	apiMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(map[string]string{"message": "Hello, World!"})
 	})
 }
