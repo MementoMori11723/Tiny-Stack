09540d8efc7c0f9e17da1e1538ebb0c5e74bd372
yasasvi <yasasvigumma@gmail.com>
2024-08-28 20:13:08 +0530

    Redesigning the structure
    let's see how it will handle.

--- client/routes.go
+++ client/routes.go
@@ -1,43 +1,11 @@
 package client
 
-import (
-	"html/template"
-	"net/http"
-)
-
-var (
-	pagesDir      = "pages/"
-	componentsDir = "components/"
-  layoutFile    = "layouts.html"
-
-	routes = Route{
-		"/":       homePage,
-		"/about/": aboutPage,
-		"/error/": errorPage,
-	}
-)
-
-type Route map[string]func(http.ResponseWriter, *http.Request)
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-  tmpl := template.Must(template.ParseFiles(pagesDir+componentsDir+layoutFile, pagesDir+"index.html"))
-  tmpl.Execute(w, nil)
-}
-
-func aboutPage(w http.ResponseWriter, r *http.Request) {
-  tmpl := template.Must(template.ParseFiles(pagesDir+componentsDir+layoutFile, pagesDir+"about.html", pagesDir+componentsDir+"contacts.html"))
-  tmpl.Execute(w, nil)
-}
-
-func errorPage(w http.ResponseWriter, r *http.Request) {
-  tmpl := template.Must(template.ParseFiles(pagesDir+componentsDir+layoutFile, pagesDir+"error.html"))
-  tmpl.Execute(w, nil)
-}
+import "net/http"
 
 func Client() *http.ServeMux {
   clientMux := http.NewServeMux()
-	for path, handler := range routes {
-		clientMux.HandleFunc(path, handler)
-	}
-	return clientMux
+  clientMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Hello, World!"))
+  })
+  return clientMux
 }
