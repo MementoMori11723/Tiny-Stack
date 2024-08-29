11fb513caba64aa114458a2cc20679a2c906f9bc
yasasvi <yasasvigumma@gmail.com>
2024-08-29 12:40:59 +0530

    Added config files
    this will configure our project!

--- app.go
+++ app.go
@@ -3,23 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
+	"github.com/MementoMori11723/Tiny-Stack/config"
 	"github.com/MementoMori11723/Tiny-Stack/server"
-	"github.com/joho/godotenv"
 )
 
 var PORT string
 
 func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	PORT = os.Getenv("PORT")
-	if PORT == "" {
-		PORT = "8080"
-	}
+	config.LoadEnv(&PORT)
 }
 
 func main() {
